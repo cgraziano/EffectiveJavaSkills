11c335cb8882664341d8132fe2d832a6bb90c6a8
cgraziano <cgraziano9363@gmail.com>
2017-03-30 21:53:04 -0600

    Through Preface

---
+++ src/Foreword.java
@@ -0,0 +1,27 @@
+/*To learn a programming language
+  Location 168
+  Understand:
+    Core language (is it algorithmmic (procedural programming), functional, object oriented)?
+      Functional: Evaluation of mathematical functions and avoids changing-state and mutable data (Lisp, Sche, Clojure, Dylan, Julia, K, Q, Excel, Scala)
+          (mutable: data that is liable to change) (A form of declarative programming, what information is desired
+          and what transformations are required.) (This is what lambdas are for in Java).
+      Object Oriented: Programming paradigm based on "objects", which may contain data, in the form of fields and
+        code, in the form of procedures, often known as methods.
+      Algorithmic (procedural) (imperative) programming: Procedures (routines, subroutines, or functions (not mathematical functions,
+        but similar to those used in functional programming)), that contain a serires of computational steps to
+        be carried out (Fortran, Algol, Cobol, Basic, Pascal, C, Ada, Go) (A developer writes code that describes in
+        exacting detail the steps that the computer must take to accomplish the goal.)
+
+      Java, c++, python, Delphi are multi-paradigm programming languages that support OO programming with
+        a combination of imperative/procedural programming.
+    What data structures, operations, facilities are provided by the standard libraries?
+    Be familiar with the customary and effective ways to structure your code.
+    Location 176
+      Grammar and vocabulary are properties of the language, but usage comes from the community that uses it.
+    Ex) Java Programming language
+      Object oriented with single inheritance
+      Supports an inperative (statement oriented) coding style within each method.
+
+
+
+
